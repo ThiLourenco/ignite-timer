09bba717f337eb20615166885e81248107af4cb9
Thigo <thiago.l17@yahoo.com>
2023-01-29 16:44:58 -0300

    Adding colors and fonts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' |'danger' | 'success';
 
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme['green-500']};
   color: ${props => props.theme.white}
 
   /* ${props =>{
