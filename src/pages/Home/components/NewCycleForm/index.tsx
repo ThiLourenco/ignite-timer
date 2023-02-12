c5efd957d54d99c48795dc7c3c6f04da051b2f84
Thigo <thiago.l17@yahoo.com>
2023-02-12 07:39:47 -0300

    feat(context): converting to context

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,5 +1,13 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
 export function NewCycleForm() {
   return (
     <FormContainer>
